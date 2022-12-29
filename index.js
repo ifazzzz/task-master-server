7f3110259732a022370ccff17d833ab54d5e1a47
ifazzzz <shoaibahmedifaz@gmail.com>
2022-12-29 20:54:44 +0600

    final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +12,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const myTasksCollection = client.db("task-master").collection("my-tasks");
-
+const completedTasks = client.db("task-master").collection("completed-tasks")
 app.get('/', (req, res) => {
     res.send('task server running')
 })
@@ -22,6 +22,29 @@ async function run(){
         app.post('/addTask', async(req, res)=> {
             const data = req.body;
             const result = await myTasksCollection.insertOne(data);
+            res.send(result)
+        })
+        app.get('/myTask', async(req, res)=> {
+            const email = req.query.email;
+            const query = {email : email};
+            const result = await myTasksCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.post('/completedTask', async(req, res)=> {
+            const task = req.body;
+            const result = await completedTasks.insertOne(task);
+            res.send(result)
+        })
+        app.get('/completedTask', async(req, res)=> {
+            const email = req.query.email;
+            const query = {email: email}
+            const result = await completedTasks.find(query).toArray();
+            res.send(result)
+        })
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id : ObjectId(id)}
+            const result = await myTasksCollection.deleteOne(filter);
             res.send(result)
         })
     }
